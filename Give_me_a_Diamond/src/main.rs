2cce5431373396bfcbac2fc0803636d5c11dfb62
iluadd <ildozh@gmail.com>
2019-09-09 01:02:20 +0300

    final solution

--- Give_me_a_Diamond/src/main.rs
+++ Give_me_a_Diamond/src/main.rs
@@ -1,35 +1,28 @@
 use std::iter;
 
 fn print(n: i32) -> Option<String> {
-    if n < 1 {
+    if n < 1 || n%2 == 0 {
         None
     } else {
-        // let mut temp = String::new();
         let mut sum = String::new();
         let fwd_numbers: Vec<i32> = (1..(n as i32)+1).collect();
         let bkw_numbers: Vec<i32> = (1..(n as i32)).rev().collect();
         let mut numbers = [&fwd_numbers[..], &bkw_numbers[..]].concat();
-        println!("{:?} - {:?}",fwd_numbers, bkw_numbers);
-        println!("{:?}",numbers);
 
         numbers.retain(|&i|i % 2 != 0);
 
-        println!("{:?}",numbers);
+        for i in 0..n {
+          sum += &" ".repeat(((n - numbers[i as usize])/2) as usize);
+          sum += &iter::repeat("*").take(numbers[i as usize] as usize).collect::<String>();
+          sum += "\n"
 
-        for i in n+1..(n+1)/2 {
-            let temp = iter::repeat("*").take(i as usize)
-                                        .collect::<String>() + "\n";
-            println!("{:?}",temp);                    
-            sum += &temp;
         }
 
-        // let sum: String = sum.to_string();
         Some(sum)
     }
 }
 
 fn main() {
-// println!("Hello, world!");
   println!("{:?}", print(5));
   println!("{:?}", print(3));
 }
@@ -44,3 +37,8 @@ fn basic_test() {
   assert_eq!(print(1), Some("*\n".to_string()) );
 }
 
+// 0 0 * 0 0 
+// 0 * * * 0
+// * * * * * 
+// 0 * * * 0
+// 0 0 * 0 0
