e94cd885c395bbc1bab4dbdfbe6ece896e355561
iluadd <ildozh@gmail.com>
2019-09-13 00:10:13 +0300

    final solution

--- Calculate_BMI/src/main.rs
+++ Calculate_BMI/src/main.rs
@@ -1,11 +1,25 @@
+fn bmi(weight: u32, height: f32) -> &'static str {
+    let mut bmi = weight as f32 / height.powi(2);
+    bmi = bmi.round();
+    match bmi {
+       bmi if bmi <= 18.5 => return "Underweight",
+       bmi if bmi <= 25.0 => return "Normal",
+       bmi if bmi <= 30.0 => return "Overweight",
+       _ => return "Obese",
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}",bmi(80, 1.80));
 }
 
 
 #[test]
 fn basic_unit_test() {
+  assert_eq!(bmi(60, 1.9), "Underweight");
   assert_eq!(bmi(80, 1.80), "Normal");
+  assert_eq!(bmi(90, 1.80), "Overweight");
+  assert_eq!(bmi(100, 1.80), "Obese");
 }
 
 
